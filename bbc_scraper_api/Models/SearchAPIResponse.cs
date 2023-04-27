77dcdd77d320faf77beafb9d29df9cef8d33db08
Foxtrot47 <jjneutron@outlook.com>
2023-04-28 00:06:40 +0530

    Parse JSON Response for Search Results

---
+++ bbc_scraper_api/Models/SearchAPIResponse.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class Item
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("postType")]
+    public string PostType { get; set; }
+
+    [JsonPropertyName("isPremium")]
+    public bool IsPremium { get; set; }
+}
+
+
+public class SearchResult
+{
+    [JsonPropertyName("items")]
+    public List<Item> Items { get; set; }
+
+    [JsonPropertyName("limit")]
+    public int Limit { get; set; }
+
+    [JsonPropertyName("totalItems")]
+    public int TotalItems { get; set; }
+
+}
+
+public class Result
+{
+    [JsonPropertyName("searchResults")]
+    public SearchResult SearchResults { get; set; }
+}
--- bbc_scraper_api/Program.cs
+++ bbc_scraper_api/Program.cs
@@ -1,3 +1,5 @@
+using RestSharp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +18,28 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+const string bbcBaseAPI = "https://www.bbcgoodfood.com/api/search-frontend/";
+const string bbbcContentAPI = "https://related-content-production.headless.imdserve.com/"; 
+var httpClient = new RestClient(new RestClientOptions(bbcBaseAPI));
+
+
+app.MapGet("/search", async (string query) => {
+    var response = await httpClient.GetJsonAsync<Result>(bbcBaseAPI + "search?search=" + query + "&limit=9999");
+    if (response != null && response.SearchResults.TotalItems > 0)
+    {
+        List<Item> items = new List<Item>(response.SearchResults.Items);
+
+        //if (response.SearchResults.TotalItems > response.SearchResults.Limit)
+        //{
+            //const int pageCount = Math.Ceiling(Convert.ToDecimal(response.SearchResults.TotalItems / response.SearchResults.Limit - 1);
+            //for (var responseLoopIndex = 0; responseLoopIndex < pageCount; responseLoopIndex++)
+            //{
+            //    var result = await httpClient.GetJsonAsync<Result>(bbcBaseAPI + "search?search=" + query)
+            //}
+        //}
+    }
+    return response;
+});
 
 app.Run();
 
